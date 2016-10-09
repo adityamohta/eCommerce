6fb254d9c81a559f8e8bca61e46e72bb4966d0d7
Kumar Aditya Mohta <kumaraditya.mohta@gmail.com>
2016-10-09 15:42:29 +0530

    Improved UI and included some homepage features.

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,32 @@ from django.contrib import admin
 
 from products.models import Product, ProductImage, Variation, Category, ProductFeatured
 
+
+class ProductImageInLine(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+    max_num = 10
+
+
+class VariationInLine(admin.TabularInline):
+    model = Variation
+    extra = 0
+    max_num = 10
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'price']
+    # list_editable = []
+    inlines = [
+        VariationInLine,
+        ProductImageInLine,
+    ]
+
+    class Meta:
+        model = Product
+
 admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductImage)
+admin.site.register(Product, ProductAdmin)
+# admin.site.register(ProductImage)
 admin.site.register(ProductFeatured)
-admin.site.register(Variation)
+# admin.site.register(Variation)
